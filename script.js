9b122782255edacbd9b038a72a4431f2420df1dc
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-09-17 15:13:10 +0530

    all file js completed

--- script.js
+++ script.js
@@ -11,3 +11,49 @@ menubtn.addEventListener("click",()=>{
 closebtn.addEventListener("click",()=>{
     sidebar.classList.remove("active")
 })
+
+
+let errorpage=document.querySelectorAll(".errorpage")
+errorpage.forEach((err)=>{
+    err.addEventListener("click",()=>{
+        window.location.href="404.html"
+    })
+})
+
+//
+
+const femail = document.querySelector("#femail");
+const footSubBtn = document.querySelector(".foot-subBtn");
+const ferror = document.querySelector(".ferror");
+
+footSubBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const emailValue = femail.value.trim();
+
+    // Email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailValue === "") {
+        ferror.textContent = "Please enter your email address";
+        ferror.style.color = "red";
+        return;
+    }
+
+    if (!emailPattern.test(emailValue)) {
+        ferror.textContent = "Please enter a valid email address";
+        ferror.style.color = "red";
+        return;
+    }
+
+    // Valid email
+    ferror.textContent = "Subscribed successfully!";
+
+    setTimeout(()=>{
+       ferror.textContent =""  
+    },3000)
+    ferror.style.color = "white";
+
+    femail.value = "";
+});
